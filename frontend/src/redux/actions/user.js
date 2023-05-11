62c8fe06a4897bf3aa1b77acd4ef09e4305143f8
sujitChougule <sujitchougule2003@gmail.com>
2023-05-12 03:05:41 +0530

    redux intergration for user

---
+++ frontend/src/redux/actions/user.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { server } from "../store";
+
+export const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "loadUserRequest",
+    });
+    const { data } = await axios.get(`${server}/me`, {
+      withCredentials: true,
+    });
+    dispatch({
+      type: "loadUserSuccess",
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: "loadUserFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
